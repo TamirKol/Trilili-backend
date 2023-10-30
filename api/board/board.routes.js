487b617c832e284488e3df28107022a4c932a324
Tal Cohen <talco4088@gmail.com>
2023-10-30 22:30:26 +0200

    start work on on the board entity server requests

---
+++ api/board/board.routes.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import { requireAuth } from '../../middlewares/requireAuth.middleware.js'
+import { log } from '../../middlewares/logger.middleware.js'
+import { getBoards, getBoardById, addBoard, updateBoard, removeBoard } from './board.controller.js'
+// import { addBoardMsg, removeBoardMsg } from './board.controller.js'
+
+const router = express.Router()
+
+// We can add a middleware for the entire router:
+// router.use(requireAuth)
+
+router.get('/', log, getBoards)
+router.get('/:id', getBoardById)
+router.post('/', requireAuth, addBoard)
+router.put('/:id', requireAuth, updateBoard)
+router.delete('/:id', requireAuth, removeBoard)
+// router.delete('/:id', requireAuth, requireAdmin, removeBoard)
+
+// router.post('/:id/msg', requireAuth, addBoardMsg)
+// router.delete('/:id/msg/:msgId', requireAuth, removeBoardMsg)
+
+export const boardRoutes = router
