95a6958d2e0e40df65ec838b961916255d2bc150
Tal Cohen <talco4088@gmail.com>
2023-11-05 15:57:08 +0200

    start work with chat gpt ai

--- server.js
+++ server.js
@@ -15,16 +15,16 @@ app.use(express.json())
 // if (process.env.NODE_ENV === 'production') {
 //     app.use(express.static(path.resolve('public')))
 // } else {
-    const corsOptions = {
-        origin: [
-            'http://127.0.0.1:3000',
-            'http://localhost:3000',
-            'http://127.0.0.1:5173',
-            'http://localhost:5173'
-        ],
-        credentials: true
-    }
-    app.use(cors(corsOptions))
+const corsOptions = {
+    origin: [
+        'http://127.0.0.1:3000',
+        'http://localhost:3000',
+        'http://127.0.0.1:5173',
+        'http://localhost:5173'
+    ],
+    credentials: true
+}
+app.use(cors(corsOptions))
 // }
 
 import { authRoutes } from './api/auth/auth.routes.js'
@@ -53,4 +53,29 @@ import { logger } from './services/logger.service.js'
 const port = process.env.PORT || 3030
 server.listen(port, () => {
     logger.info('Server is running on port: ' + port)
+})
+
+
+
+////////////////////ghat gpt
+import { OpenAI } from "openai";
+import { CHAT_GPT_KEY, CHAT_GPT_KEY3 } from './services/apiKeys.js'
+import bodyParser from 'body-parser'
+import { log } from 'console'
+
+const openai = new OpenAI({
+    apiKey: CHAT_GPT_KEY3 // This is also the default, can be omitted
+});
+
+app.use(bodyParser.json())
+
+app.post('/chat', async (req, res) => {
+    console.log(req.body);
+    const { prompt } = req.body
+    const completion = await openai.completions.create({
+        model: 'gpt-3.5-turbo-instruct',
+        prompt: prompt
+    });
+
+    res.send(completion.data.choices[0].text)
 })
